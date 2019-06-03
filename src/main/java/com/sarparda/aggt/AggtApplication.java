4fbc0ae3cd0178ab889043d569dc2407c045a011
sarparda <akif.ova@gmail.com>
2019-06-04 00:15:47 +0300

    Initial Commit

---
+++ src/main/java/com/sarparda/aggt/AggtApplication.java
@@ -0,0 +1,13 @@
+package com.sarparda.aggt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggtApplication.class, args);
+    }
+
+}
